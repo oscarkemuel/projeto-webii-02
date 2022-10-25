454f0a7ce763f9b22a0fbcdb9dc98b673d7f013f
Oscar Kemuel Orrico dos Santos <oscar.kemuel5@gmail.com>
2022-10-25 20:10:57 -0300

    feat: initial auth configuration

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeSave, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
+import LinkToken from './LinkToken'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -14,9 +16,24 @@ export default class User extends BaseModel {
   @column()
   public phone: string
 
+  @column({ serializeAs: null })
+  public password: string
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => LinkToken, {
+    foreignKey: 'userId',
+  })
+  public tokens: HasMany<typeof LinkToken>
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 }
--- app/Services/UserService.ts
+++ app/Services/UserService.ts
@@ -5,6 +5,7 @@ interface UserInterface {
   name: string
   email: string
   phone: string
+  password: string
 }
 
 class UserService {
